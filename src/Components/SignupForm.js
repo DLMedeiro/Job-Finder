ca2fc1b4d51e8c50459f4138c85ad80987ef62ec
DanielleM <dlmedeiro@gmail.com>
2023-05-25 20:02:05 -0700

    condence form layout

--- src/Components/SignupForm.js
+++ src/Components/SignupForm.js
@@ -7,6 +7,8 @@ import {
   Button,
   FormGroup,
   Form,
+  Col,
+  Row,
 } from "reactstrap";
 
 function SignupForm({ registerNewUser }) {
@@ -38,20 +40,74 @@ function SignupForm({ registerNewUser }) {
       <h1>Sign Up</h1>
       <CardBody className="text-center">
         <Form onSubmit={submit}>
-          <FormGroup>
-            <Label className="form-label" htmlFor="username">
-              Username
-            </Label>
-            <Input
-              className="form-control"
-              id="username"
-              required="required"
-              type="text"
-              name="username"
-              value={formData.username}
-              onChange={handleChange}
-            />
-          </FormGroup>
+          <Row>
+            <Col md={6}>
+              <FormGroup>
+                <Label className="form-label" htmlFor="username">
+                  Username
+                </Label>
+                <Input
+                  className="form-control"
+                  id="username"
+                  required="required"
+                  type="text"
+                  name="username"
+                  value={formData.username}
+                  onChange={handleChange}
+                />
+              </FormGroup>
+            </Col>
+            <Col md={6}>
+              <FormGroup>
+                <Label className="form-label" htmlFor="email">
+                  Email
+                </Label>
+                <Input
+                  className="form-control"
+                  id="email"
+                  required="required"
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+          <Row>
+            <Col md={6}>
+              <FormGroup>
+                <Label className="form-label" htmlFor="firstName">
+                  First Name
+                </Label>
+                <Input
+                  className="form-control"
+                  id="firstName"
+                  required="required"
+                  type="text"
+                  name="firstName"
+                  value={formData.firstName}
+                  onChange={handleChange}
+                />
+              </FormGroup>
+            </Col>
+            <Col md={6}>
+              <FormGroup>
+                <Label className="form-label" htmlFor="lastName">
+                  Last Name
+                </Label>
+                <Input
+                  className="form-control"
+                  id="lastName"
+                  required="required"
+                  type="text"
+                  name="lastName"
+                  value={formData.lastName}
+                  onChange={handleChange}
+                />
+              </FormGroup>
+            </Col>
+          </Row>
           <FormGroup>
             <Label className="form-label" htmlFor="password">
               Password
@@ -63,48 +119,6 @@ function SignupForm({ registerNewUser }) {
               type="password"
               name="password"
               value={formData.password}
-              onChange={handleChange}
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label className="form-label" htmlFor="firstName">
-              First Name
-            </Label>
-            <Input
-              className="form-control"
-              id="firstName"
-              required="required"
-              type="text"
-              name="firstName"
-              value={formData.firstName}
-              onChange={handleChange}
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label className="form-label" htmlFor="lastName">
-              Last Name
-            </Label>
-            <Input
-              className="form-control"
-              id="lastName"
-              required="required"
-              type="text"
-              name="lastName"
-              value={formData.lastName}
-              onChange={handleChange}
-            />
-          </FormGroup>
-          <FormGroup>
-            <Label className="form-label" htmlFor="email">
-              Email
-            </Label>
-            <Input
-              className="form-control"
-              id="email"
-              required="required"
-              type="email"
-              name="email"
-              value={formData.email}
               onChange={handleChange}
             />
           </FormGroup>
